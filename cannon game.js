7c2bab545de9534b9b474e41a79453e3a5e4d706
lomarbrick <132483874+lomarbrick@users.noreply.github.com>
2023-05-09 11:43:25 -0500

    Update cannon game.js

--- cannon game.js
+++ cannon game.js
@@ -128,10 +128,10 @@ class Building{
                 var distDiff=getDistance(this.points[i],this.points[connectedIndex])-this.distances[i][connectedIndex];
                 if(i!=j && distDiff!=0){
                     var angle=getAngle(this.points[i],this.points[connectedIndex]);
-                    this.points[i].vx+=Math.cos(angle+Math.PI)*distDiff*2.5/this.points.length;
-                    this.points[i].vy+=Math.sin(angle+Math.PI)*distDiff*2.5/this.points.length;
-                    this.points[connectedIndex].vx+=Math.cos(angle)*distDiff*2.5/this.points.length;
-                    this.points[connectedIndex].vy+=Math.sin(angle)*distDiff*2.5/this.points.length;
+                    this.points[i].vx+=Math.cos(angle+Math.PI)*distDiff*3/this.points.length;
+                    this.points[i].vy+=Math.sin(angle+Math.PI)*distDiff*3/this.points.length;
+                    this.points[connectedIndex].vx+=Math.cos(angle)*distDiff*3/this.points.length;
+                    this.points[connectedIndex].vy+=Math.sin(angle)*distDiff*3/this.points.length;
                 }
             }
             this.points[i].x+=this.points[i].vx;
