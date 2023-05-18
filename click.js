8a8329e31a22b51191767fcade6faeafa71e911f
lomarbrick <132483874+lomarbrick@users.noreply.github.com>
2023-05-18 12:12:23 -0500

    Update click.js

--- click.js
+++ click.js
@@ -2,7 +2,7 @@ var button=document.getElementById("click");
 var clickText=document.getElementById("clickCount");
 var oldClicks=document.cookie.split("=")
 var clicks=0;
-if(oldClicks.length>0){
+if(oldClicks.length>0 && oldClicks[0]=="clicks"){
     clicks=parseInt(oldClicks[1]);
 }
 clickText.innerHTML="You have clicked "+clicks+" times";
