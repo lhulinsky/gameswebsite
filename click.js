8ad898377c0dac1298f6a5bd7d05386ebd23410c
lomarbrick <132483874+lomarbrick@users.noreply.github.com>
2023-05-17 20:53:16 -0500

    Update click.js

--- click.js
+++ click.js
@@ -1,3 +1,4 @@
+var button=document.getElementById("click");
 console.log(document.cookie);
 var clicks=0;
 function click(){
@@ -5,3 +6,4 @@ function click(){
     clicks+=1;
     document.cookie="clicks="+clicks+";expires="+(new Date().getTime()+24*60*60*1000);
 }
+button.onclick="click()";
