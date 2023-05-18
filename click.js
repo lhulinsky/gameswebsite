7b61e86234ba6eb9d49587d55fae0449f9c98237
lomarbrick <132483874+lomarbrick@users.noreply.github.com>
2023-05-18 14:00:05 -0500

    Update click.js

--- click.js
+++ click.js
@@ -1,9 +1,16 @@
 var button=document.getElementById("click");
 var clickText=document.getElementById("clickCount");
+var canvas=document.getElementById("cursorCanvas");
+var ctx=canvas.getContext("2d");
+var cursorImage=new Image();
+cursorImage.src="cursor.png";
 var oldClicks=document.cookie.split("=")
 var clicks=0;
 if(oldClicks.length>0 && oldClicks[0]=="clicks"){
     clicks=parseInt(oldClicks[1]);
+    for(var i=0;i<clicks;i++){
+        ctx.drawImage(cursorImage,Math.random()*canvas.width,Math.random()*canvas.height);
+    }
 }
 clickText.innerHTML="You have clicked "+clicks+" times";
 function clickCookie(){
@@ -11,4 +18,5 @@ function clickCookie(){
     clicks+=1;
     document.cookie="clicks="+clicks;
     clickText.innerHTML="You have clicked "+clicks+" times";
+    ctx.drawImage(cursorImage,Math.random()*canvas.width,Math.random()*canvas.height);
 }
