53a56cebfe078b5fb8dd0d9036ef8f56cdaa4423
lhulinsky <78326398+lhulinsky@users.noreply.github.com>
2023-09-01 10:46:17 -0500

    circle test

--- bubble optimization.js
+++ bubble optimization.js
@@ -4,48 +4,32 @@ precision highp float;
 
 in vec4 position;
 out vec2 screenPosition;
-uniform float zoom;
 uniform vec2 u_screenSize;
-uniform vec2 u_origin;
 out float time;
 
 void main()
 {
     gl_Position = position;
-    screenPosition=vec2(position.x*(u_screenSize.x/u_screenSize.y)*zoom+u_origin.x,position.y*zoom+u_origin.y);
+    screenPosition=vec2(position.x*(u_screenSize.x/u_screenSize.y),position.y);
 }`;
 
 var fragmentShaderText =
 `#version 300 es
-precision highp float;
+precision medp float;
 in vec2 screenPosition;
 out vec4 outColor;
-float maxIterations=100.0;
 
-vec2 squareImaginary(vec2 number){
-	return vec2(
-		number.x*number.x-number.y*number.y,
-		2.0*number.x*number.y
-	);
-}
-
-float iterateMandelbrot(vec2 coord){
-	vec2 z = vec2(0,0);
-	for(float i=0.0;i<maxIterations;i++){
-		z = squareImaginary(z) + coord;
-		if(length(z)>2.0) return i/maxIterations;
-	}
-	return maxIterations;
+float distance(vec2 pos){
+    return sqrt(pow(pos.x,2)+pow(pos.y,2));
 }
 
 void main()
 {
-    float color=1.0-iterateMandelbrot(screenPosition);
-    if(color>.1){
-        outColor = vec4((color*10.0)*(color*10.0),color*.8,0,1);
+    if(distance(screenPosition)<.4){
+        outColor = vec4(1,1,1,1);
     }
     else{
-        outColor = vec4(color,color,color,1);
+        outColor = vec4(0,0,0,1);
     }
 }`;
 function runShader(){
@@ -120,42 +104,17 @@ function runShader(){
 	);
 
 	gl.bindVertexArray(null);
-	var zoomUniformLocation=gl.getUniformLocation(program,"zoom");
 	var zoom=1.1;
 	var screenSizeUniformLocation=gl.getUniformLocation(program,"u_screenSize");
-	var originUniformLocation=gl.getUniformLocation(program,"u_origin");
 	var origin=[0,0];
 	function loop(){
 		gl.clearColor(0.0,0.0,0.0,1.0);
 		gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
-		gl.uniform1f(zoomUniformLocation,zoom);
 		gl.uniform2f(screenSizeUniformLocation,width,height);
-		gl.uniform2f(originUniformLocation,origin[0],origin[1]);
 		gl.bindVertexArray(backgroundVertexArray);
 		gl.drawArrays(gl.TRIANGLES, 0, 6);
 		gl.bindVertexArray(null);
 		requestAnimationFrame(loop);
-	}
-	document.onwheel=function scale(e){
-	    if(e.deltaY>0){
-	        zoom*=1.1;
-	    }
-	    else{
-	        zoom*=.9;
-	    }
-	}
-	var dragging=false;
-	document.body.onmousedown=function(e){
-	    dragging=true;
-	}
-	document.body.onmouseup=function(e){
-	    dragging=false;
-	}
-	document.body.onmousemove=function(e){
-	    if(dragging){
-	        origin[0]-=e.movementX*zoom/(width/2)*(width/height);
-	        origin[1]+=e.movementY*zoom/(height/2);
-	    }
 	}
 
 	requestAnimationFrame(loop);
