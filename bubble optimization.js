139ba5baddae4def1c14dcbba30cf0f2dd803f86
lhulinsky <78326398+lhulinsky@users.noreply.github.com>
2023-09-02 10:15:30 -0500

    color change debugging

--- bubble optimization.js
+++ bubble optimization.js
@@ -107,7 +107,7 @@ function drawFancyCircles(circlePositions,filled,siteColor){
 	gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 	gl.uniform2f(screenSizeUniformLocation,headerCanvas.width,headerCanvas.height);
 	//hsl string to rgb
-	var hueValue=siteColor.split(",").slice(4);
+	var hueValue=siteColor.split(",");
 	console.log(hueValue);
 	var hueNumber=parseInt(hueValue);
 	var rgbColor=hslToRgb(hueNumber,100,50);
