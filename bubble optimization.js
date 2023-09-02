5c0dd25ef39331b7603c4cf1e8c7cff03652d60f
lhulinsky <78326398+lhulinsky@users.noreply.github.com>
2023-09-02 10:10:17 -0500

    color changing header

--- bubble optimization.js
+++ bubble optimization.js
@@ -19,15 +19,16 @@ precision mediump float;
 in vec2 screenPosition;
 out vec4 outColor;
 uniform int circleMode;
+uniform vec3 u_siteColor;
 
 void main()
 {
 	float circleDistance=length(gl_PointCoord-vec2(.5,.5));
 	if(circleMode==0 && circleDistance<.5 && circleDistance>.47){
-		outColor = vec4(1,0,0,1);
+		outColor = vec4(u_siteColor,1);
 	}
 	else if(circleMode==1 && circleDistance<.5){
-		outColor = vec4(1,0,0,.3);
+		outColor = vec4(u_siteColor,.3);
 	}
 	else{
 		discard;
@@ -74,10 +75,42 @@ gl.useProgram(program);
 gl.bindVertexArray(null);
 var screenSizeUniformLocation=gl.getUniformLocation(program,"u_screenSize");
 var circleModeUniformLocation=gl.getUniformLocation(program,"circleMode");
-function drawFancyCircles(circlePositions,filled){
+var siteColorUniformLocation=gl.getUniformLocation(program,"u_siteColor");
+
+function hslToRgb(h, s, l) {
+	let r, g, b;
+  
+	if (s === 0) {
+	  r = g = b = l; // achromatic
+	} else {
+	  const q = l < 0.5 ? l * (1 + s) : l + s - l * s;
+	  const p = 2 * l - q;
+	  r = hueToRgb(p, q, h + 1/3);
+	  g = hueToRgb(p, q, h);
+	  b = hueToRgb(p, q, h - 1/3);
+	}
+  
+	return [r, g, b];
+  }
+  
+  function hueToRgb(p, q, t) {
+	if (t < 0) t += 1;
+	if (t > 1) t -= 1;
+	if (t < 1/6) return p + (q - p) * 6 * t;
+	if (t < 1/2) return q;
+	if (t < 2/3) return p + (q - p) * (2/3 - t) * 6;
+	return p;
+  }
+
+function drawFancyCircles(circlePositions,filled,siteColor){
 	gl.clearColor(1.0,1.0,1.0,1.0);
 	gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 	gl.uniform2f(screenSizeUniformLocation,headerCanvas.width,headerCanvas.height);
+	//hsl string to rgb
+	var hueValue=siteColor.split(",").slice(4);
+	var hueNumber=parseInt(hueValue)/255;
+	var rgbColor=hslToRgb(hueNumber,100,50);
+	gl.uniform3fv(siteColorUniformLocation,rgbColor);
 	if(filled){
 		gl.uniform1i(circleModeUniformLocation,1);
 	}
