ea49f4e3120332e5496133d538c556733c025b8c
lhulinsky <78326398+lhulinsky@users.noreply.github.com>
2023-10-15 14:13:18 -0500

    central bell offset script

--- school bell offset.js
+++ school bell offset.js
@@ -1,1 +1,7 @@
-
+function getSchoolBellOffset(){
+    var startDate=new Date("August 24, 2023 0:00:00")
+    var millisecondsPassed=new Date().getTime()-startDate.getTime();
+    const originalOffset=38;
+    const offsetPerDay=.3;
+    return orgininalOffset+offsetPerDay*Math.floor(millisecondsPassed/(1000*60*60*24));
+}
