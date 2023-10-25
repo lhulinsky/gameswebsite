2ba71f90c86fb479caab7036dba02f400e027d10
lhulinsky <78326398+lhulinsky@users.noreply.github.com>
2023-10-25 16:20:48 -0500

    what

--- tic tac toe.js
+++ tic tac toe.js
@@ -114,10 +114,11 @@ function miniMax(depthLeft,player){
             if(player==1){
                 nextPlayer=2;
             }
-            var score=miniMax(depthLeft-1,nextPlayer);
+            var score=-miniMax(depthLeft-1,nextPlayer);
             if(score>bestScore){
                 bestScore=score;
                 if(depthLeft==9){
+                    console.log(score);
                     bestMove=i;
                 }
             }
