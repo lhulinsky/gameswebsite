8b6a482f5465cd15e0b326067fb0d18bacfb0210
lhulinsky <78326398+lhulinsky@users.noreply.github.com>
2023-11-06 10:18:52 -0600

    bell offset

---
+++ math solver.js
@@ -0,0 +1,7 @@
+var solutionText=document.getElementById("solutionText");
+var numbers=[2,3,4,9,25,120];
+function findSolution(){
+    
+    requestAnimationFrame(findSolution);
+}
+requestAnimationFrame(findSolution);
