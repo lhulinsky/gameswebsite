298a716745ddd2d13f47e42122218b1a904962d5
lhulinsky <78326398+lhulinsky@users.noreply.github.com>
2023-12-11 18:09:01 -0600

    i

--- molecular shape.js
+++ molecular shape.js
@@ -61,6 +61,7 @@ function draw(){
     var sortedAtoms=atoms.slice();
     for(var i=0;i<sortedAtoms.length-1;i++){
         if(sortedAtoms[i].position.z>sortedAtoms[i+1].position.z){
+            console.log(i);
             firstAtom=sortedAtoms[i];
             sortedAtoms[i]=sortedAtoms[i+1];
             sortedAtoms[i+1]=firstAtom;
