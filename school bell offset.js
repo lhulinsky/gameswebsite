2a773208c779621ff3dd78e7f14b66f7dabebea0
lhulinsky <78326398+lhulinsky@users.noreply.github.com>
2024-10-29 11:22:57 -0500

    bell offset

--- school bell offset.js
+++ school bell offset.js
@@ -2,6 +2,6 @@ function getSchoolBellOffset(){
     var startDate=new Date("Aug 30, 2024 0:00:00")
     var millisecondsPassed=new Date().getTime()-startDate.getTime();
     const originalOffset=-4;
-    const offsetPerDay=.5;
+    const offsetPerDay=.365;
     return originalOffset+Math.floor(offsetPerDay*millisecondsPassed/(1000*60*60*24));
 }
